11f713f78ef93685d01a7565e47b63773a6f5dac
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-03-10 20:35:15 +0530

    [MODIFIED]:Dipak - UC-1 Completed For Every Day stake And Every Per Bet

--- src/com/dipak/gamblingsimulationproblem/GamblingSimulation.java
+++ src/com/dipak/gamblingsimulationproblem/GamblingSimulation.java
@@ -1,11 +1,20 @@
 package com.dipak.gamblingsimulationproblem;
 
 public class GamblingSimulation {
-
-	public static void main(String[] args) {
+	
+	private final static int Every_day_stake = 100;
+	private final static int Every_day_bet = 1;
+	
+	public static void gamblingshow(){
+		System.out.println("Every Day Stake:"+Every_day_stake);
+		System.out.println("Every Per Bet:"+Every_day_bet);
+}
+public static void main(String[] args) {
 		
 		System.out.println("Welcome To Gambling Simulation Project");
-
+		
+		GamblingSimulation gambling = new GamblingSimulation();
+		gambling.gamblingshow();
 	}
 
 }
