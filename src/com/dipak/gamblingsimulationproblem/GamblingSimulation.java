f22eb22ff9c158c8598d8ff23e984d66ce2b3566
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-03-10 17:30:41 +0530

    [ADDED]:Dipak - Gambling Simulation Project Setup

---
+++ src/com/dipak/gamblingsimulationproblem/GamblingSimulation.java
@@ -0,0 +1,11 @@
+package com.dipak.gamblingsimulationproblem;
+
+public class GamblingSimulation {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome To Gambling Simulation Project");
+
+	}
+
+}
